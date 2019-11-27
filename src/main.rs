951c80b8f66f2758f1de715df79a6446723b9abf
Richard Dallaway <richard@dallaway.com>
2019-11-27 12:25:50 +0000

    reworked Tim's example to use schedule

--- src/main.rs
+++ src/main.rs
@@ -6,67 +6,120 @@ extern crate panic_halt;
 use crate::hal::{prelude::*, stm32};
 use stm32f4xx_hal as hal;
 
-// Imports to reduce length of type signatures in the code
-use hal::gpio::gpioa::PA5;
-use hal::gpio::{Output, PushPull};
+use crate::ws2812::Ws2812;
+use ws2812_spi as ws2812;
 
-use cortex_m::iprint;
+use smart_leds::SmartLedsWrite;
+use smart_leds_trait::RGB8;
+
+use hal::spi::*;
+
+use cortex_m::iprintln;
+use cortex_m_semihosting::hprintln;
 
 use rtfm::cyccnt::U32Ext;
 
 const PERIOD: u32 = 48_000_000;
+const NUM_LEDS: usize = 4;
+
+// Types for WS
+use hal::stm32::SPI1;
+use stm32f4xx_hal::gpio::gpiob::{PB3, PB5};
+use stm32f4xx_hal::gpio::{Alternate, AF5};
+use stm32f4xx_hal::spi::{NoMiso, Spi};
+
+type Pins = (PB3<Alternate<AF5>>, NoMiso, PB5<Alternate<AF5>>);
 
 #[rtfm::app(device = stm32f4xx_hal::stm32, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
-        led: PA5<Output<PushPull>>,
-        is_on: bool,
+        ws: Ws2812<Spi<SPI1, Pins>>,
         itm: cortex_m::peripheral::ITM,
     }
 
-    #[init(schedule = [blinky])]
+    #[init(schedule = [lights_on])]
     fn init(mut cx: init::Context) -> init::LateResources {
         // Device specific peripherals
         let dp: stm32::Peripherals = cx.device;
 
-        // Set up the LED: it's connected to pin PA5 on the microcontroler
-        let gpioa = dp.GPIOA.split();
-        let led = gpioa.pa5.into_push_pull_output();
-
-        // Set up the system clock. We want to run at 48MHz for this one.
+        // Set up the system clock at 48MHz
         let rcc = dp.RCC.constrain();
-        let _clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
 
         // Initialize (enable) the monotonic timer (CYCCNT)
-        // on the Cortex-M peripherals
         cx.core.DCB.enable_trace();
         cx.core.DWT.enable_cycle_counter();
+
+        // ITM for debugging output
         let itm = cx.core.ITM;
 
-        cx.schedule.blinky(cx.start + PERIOD.cycles()).expect("ooh");
+        // Configure pins for SPI
+        // We don't connect sck, but I think the SPI traits require it?
+        let gpiob = dp.GPIOB.split();
+        let sck = gpiob.pb3.into_alternate_af5();
 
-        init::LateResources {
-            led,
-            is_on: false,
-            itm,
-        }
+        // Master Out Slave In - pb5, Nucleo 64 pin d4
+        let mosi = gpiob.pb5.into_alternate_af5();
+
+        let spi = Spi::spi1(
+            dp.SPI1,
+            (sck, NoMiso, mosi),
+            Mode {
+                polarity: Polarity::IdleLow,
+                phase: Phase::CaptureOnFirstTransition,
+            },
+            stm32f4xx_hal::time::KiloHertz(3000).into(),
+            clocks,
+        );
+
+        let ws = Ws2812::new(spi);
+
+        cx.schedule
+            .lights_on(cx.start + PERIOD.cycles())
+            .expect("failed schedule initial lights on");
+
+        init::LateResources { ws, itm }
     }
 
-    #[task(schedule = [blinky], resources = [led, itm, is_on])]
-    fn blinky(cx: blinky::Context) {
-        let is_on: &mut bool = cx.resources.is_on;
-        let led = cx.resources.led;
+    #[task(schedule = [lights_off], resources = [ws, itm])]
+    fn lights_on(cx: lights_on::Context) {
+        let lights_on::Resources { ws, itm } = cx.resources;
+        let port = &mut itm.stim[0];
 
-        if *is_on {
-            led.set_high().unwrap();
-        } else {
-            led.set_low().unwrap();
-        }
-        *is_on = !(*is_on);
+        iprintln!(port, "ON");
+
+        let blue = RGB8 {
+            b: 0xa0,
+            g: 0,
+            r: 0,
+        };
+        let data = [blue; NUM_LEDS];
+
+        ws.write(data.iter().cloned())
+            .expect("Failed to write lights_on");
 
         let next = cx.scheduled + PERIOD.cycles();
-        iprint!(&mut cx.resources.itm.stim[0], "{:?}", next);
-        cx.schedule.blinky(next).unwrap();
+        cx.schedule
+            .lights_off(next)
+            .expect("Failed to schedule lights_off");
+    }
+
+    #[task(schedule = [lights_on], resources = [ws, itm])]
+    fn lights_off(cx: lights_off::Context) {
+        let lights_off::Resources { ws, itm } = cx.resources;
+        let port = &mut itm.stim[0];
+
+        hprintln!("OFF").unwrap();
+        iprintln!(port, "OFF");
+
+        let empty = [RGB8::default(); NUM_LEDS];
+        ws.write(empty.iter().cloned())
+            .expect("Failed to write lights_off");
+
+        let next = cx.scheduled + PERIOD.cycles();
+        cx.schedule
+            .lights_on(next)
+            .expect("Failed to schedule lights_on");
     }
 
     extern "C" {
